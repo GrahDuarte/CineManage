3d15827308530e8a755212e7fcdd2fa7e527cdba
Claudio Alcantara <54650277+ClaudioJSA@users.noreply.github.com>
2023-06-01 23:02:17 -0300

    Update CinemaDao.java
    Estava sendo passado uma classe ao invés de uma chave estrangeira para o sql do bd.

--- CRUD_Cinema/src/main/java/models/Daos/CinemaDao.java
+++ CRUD_Cinema/src/main/java/models/Daos/CinemaDao.java
@@ -72,7 +72,7 @@ public class CinemaDao extends Dao<Cinema> {
 
                 pstmt.setString(1, e.getNomeFantasia());
                 pstmt.setLong(2, e.getCnpj());
-                pstmt.setObject(3, e.getEndereco());
+                pstmt.setObject(3, NULL);
                 pstmt.setLong(4, e.getTelefone());
                 pstmt.setString(5, e.getSigla());
                 
