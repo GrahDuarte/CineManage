115e62e6394d8dc7502890897f4a5d7ea5125a51
GrahDuarte <119550289+GrahDuarte@users.noreply.github.com>
2023-06-24 13:59:09 -0300

    Add files via upload

---
+++ CRUD_Cinema/src/main/java/models/Classes/Bomboniere.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Graciely Duarte
+ */
+public class Bomboniere {
+    private ArrayList<Produto> produto;
+    private int venda;
+
+    public Sala() {
+        produto = new Produto<>();
+    }
+
+    public Bomboniere(ArrayList<Produto> produto, int venda) {
+        this.produto = produto;
+        this.venda = venda;
+    }
+
+ public ArrayList<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(ArrayList<Produto> produto) {
+        this.produto = produto
+    }
+
+    public int getVenda() {
+        return venda;
+    }
+
+    public void setVenda(int venda) {
+        this.venda = venda;
+    }
+    
+    void adicionarProduto(Produto produto){
+
+    }
+    void removerProduto(Sessao produto){
+
+    }
+
+    void venderProduto(){
+
+    }
+
+   
