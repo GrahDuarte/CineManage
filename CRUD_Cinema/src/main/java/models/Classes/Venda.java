9684736c90dbc197fd51ce0e38556337e5fe6eae
GrahDuarte <119550289+GrahDuarte@users.noreply.github.com>
2023-06-24 14:42:02 -0300

    Add files via upload

---
+++ CRUD_Cinema/src/main/java/models/Classes/Venda.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models.Classes;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Graciely Duarte
+ */
+public class Venda {
+    private ArrayList<Produto> produto;
+    private LocalDateTime emissaoDaVenda;
+    private BigDecimal precoTotal;
+
+    public Venda() {
+        produto = new Produto<>();
+    }
+
+    public Venda(ArrayList<Produto> produto, LocalDateTime emissaoDaVenda, BigDecimal precoTotal) {
+        this.produto = produto;
+        this.emissaoDaVenda = emissaoDaVenda;
+        this.precoTotal = precoTotal;
+    }
+  
+    public ArrayList<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(ArrayList<Produto> produto) {
+        this.produto = produto;
+    }
+
+    public LocalDateTime getEmissaoDaVenda() {
+        return emissaoDaVenda;
+    }
+
+    public void setEmissaoDaVenda(LocalDateTime emissaoDaVenda) {
+        this.emissaoDaVenda = emissaoDaVenda;
+    }
+    
+   public BigDecimal getPrecoTotal() {
+        return precoTotal;
+    }
+
+    public void setPrecoTotal(BigDecimal precoTotal) {
+        this.precoTotal = precoTotal;
+    }
